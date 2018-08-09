acb4bbabc5254ee8d34303f3e3b8f06900f3b4c3
eloisa froes <ola.eloisa@gmail.com>
2018-08-09 17:49:41 -0300

    created view for the concktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Cocktail.create(name: "bloody mary")
+Cocktail.create(name: "caipirinha")
+Cocktail.create(name: "michelada")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "tomato juice")
+Ingredient.create(name: "syrup")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "soda")
+Ingredient.create(name: "tonic")
+Ingredient.create(name: "salt")
+Ingredient.create(name: "beer")
+Ingredient.create(name: "rosemary sprigs")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "cachaça")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "orange bitters")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "strawberry schnapps")
+Ingredient.create(name: "sake")
+Ingredient.create(name: "passion fruit")
+Ingredient.create(name: "grenadine")
